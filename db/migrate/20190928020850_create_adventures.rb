e7707d5c4db032e55ecf933e287718a1d0fab9e9
Khris Punzalan <khrispunzalan@gmail.com>
2019-09-27 19:47:44 -0700

    created table for adventures

--- db/migrate/20190928020850_create_adventures.rb
+++ db/migrate/20190928020850_create_adventures.rb
@@ -1,4 +1,12 @@
 class CreateAdventures < ActiveRecord::Migration
   def change
+    create_table :adventures do |t|
+      t.string :title
+      t.string :location
+      t.string :activity
+      t.string :companion
+      t.datetime  :created_at
+      t.datetime  :updated_at
+    end 
   end
 end
