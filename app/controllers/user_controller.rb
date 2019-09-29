353533863979165d9c429a2825fb2d42252b0061
Khris Punzalan <khrispunzalan@gmail.com>
2019-09-29 14:39:35 -0700

    created email validation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
     has_many :adventures
     has_secure_password
+
+    # must have validations
+    validates :email, presence: { message: "Please enter your email." }
+    
 end
