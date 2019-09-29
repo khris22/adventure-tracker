d6ceb6655582b6ff1536a7058d57d54909d09c97
Khris Punzalan <khrispunzalan@gmail.com>
2019-09-29 14:31:23 -0700

    created dummy user data

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+steve = User.create(username: "steve_04", email:"steve@mail.com", password:"stevedc")
+khris = User.create(username: "Kp12", email:"khris@yahoo.com", password:"forgotten")
+boris = User.create(username: "B_o_R_i_S", email:"boris@hotmail.com", password:"GiveMeTreats")
+
