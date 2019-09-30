6a644eb3766ea123ce5cb207976adc6da269b0c4
Khris Punzalan <khrispunzalan@gmail.com>
2019-09-29 18:25:38 -0700

    added validation to user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,17 @@ class User < ActiveRecord::Base
     # must have validations
     # validates :email, presence: { message: "Please enter your email." }
     
-    validates(:email,
+    validates (:email,
                 :presence   => {:message => "Email can't be blank"},
                 :uniqueness => {:message => 'Email must be unique'},
                 :email      => {:message => 'Email must be valid'})
     # https://www.rubydoc.info/gems/active-model-email-validator/1.0/EmailValidator
 
+    validates :username, uniqueness: {
+      message: ->(object, data) do
+        "Hey #{object.name}!, #{data[:value]} is taken already!"
+      end
+    }
 
+    validates :password, presence: true #length: { in: 6..20, }
 end
