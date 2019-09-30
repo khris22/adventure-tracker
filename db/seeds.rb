d72ef98f815c1343297a4827c23c59bf5755e00e
Khris Punzalan <khrispunzalan@gmail.com>
2019-09-30 04:27:52 -0700

    updated data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@ steve = User.create(username: "steve_04", email:"steve@mail.com", password:"stev
 khris = User.create(username: "Kp12", email:"khris@yahoo.com", password:"forgotten")
 boris = User.create(username: "B_o_R_i_S", email:"boris@hotmail.com", password:"GiveMeTreats")
 
-grandcanyon = Adventure.create 
-:title 
-:location 
-:activity 
-:companion
-:created_at
-:updated_at
-:user_id
 
 
-hiking
+grandcanyon = Adventure.create( 
+title: "Grand Canyon Adventure" , 
+location: "Arizona", 
+activity: "Hiking & Camping", 
+companion: "Khris",
+created_at: "",
+updated_at: "",
+user_id: )
+
