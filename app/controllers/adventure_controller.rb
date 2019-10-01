e2b7951fa6b7c8649e3e569fe483d0ca8f2f380b
Khris Punzalan <khrispunzalan@gmail.com>
2019-10-01 05:14:24 -0700

    login route working and showing each adventure

--- app/controllers/adventure_controller.rb
+++ app/controllers/adventure_controller.rb
@@ -4,6 +4,7 @@ class AdventuresController < ApplicationController
         if logged_in?
             # binding.pry
             @user = current_user
+            # @adventure = current_user.adventures
             erb :"/adventures/index"
         else
             redirect '/'
@@ -11,12 +12,23 @@ class AdventuresController < ApplicationController
         end
     end
 
+    get '/adventures/:id' do
+        if logged_in?
+            @user = current_user
+            @adventure = Adventure.find_by(params[:id])
+            erb :"/adventures/show"
+        else
+            redirect '/adventures'
+        end
+    end
+
     post '/adventures' do
         if logged_in?
+            @user = User.find_by_id(params[:user_id])
             if params[:title].empty? && params[:location].empty?
                 redirect '/adventures'
             else
-                @adventure = current_user.adventures.build(params)
+                @adventure = current_user.adventures.build(title: params[:title], location: params[:location], activity: params[:activity], companion: params[:companion], notes: params[:notes])
                 if @adventure.save
                     redirect "/adventures/#{@adventure.id}"
                 else
@@ -26,14 +38,7 @@ class AdventuresController < ApplicationController
         end
     end
 
-    # get '/adventures/index' do
-    #     if logged_in?
-    #         @user = current_user
-    #         erb :"/adventures/index"
-    #     else
-    #         redirect '/'
-    #     end
-    # end
+ 
 
 end
 
