8e7fdf975785ac6b71e91aeb6ebd22b943b0a238
Khris Punzalan <khrispunzalan@gmail.com>
2019-10-02 05:26:20 -0700

    moved routes to the appropriate controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,24 +15,7 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  get '/signup' do
-    erb :"/user/signup"
-  end
-
-  post '/user' do
-    user = User.new(params)
-    # user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    if user.save
-      session[:user_id] = user.id
-      redirect '/adventures'
-      # "/welcome"
-      # redirect to login page, message your account has been created please log in
-    else
-      redirect '/signup'
-      #Please try again (make sure all the details have been filled)
-    end
-  end
-
+  
   post '/login' do
     user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
