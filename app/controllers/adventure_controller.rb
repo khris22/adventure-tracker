1a6fac0384c1bc263e2a559b43a754a01e2f93f4
Khris Punzalan <khrispunzalan@gmail.com>
2019-10-01 20:50:44 -0700

    debugged and created edit 'patch' route

--- app/controllers/adventure_controller.rb
+++ app/controllers/adventure_controller.rb
@@ -48,13 +48,36 @@ class AdventuresController < ApplicationController
                 # binding.pry
                 erb :"/adventures/edit"
             else
-                redirect :"/adventures/#{@adventure.id}"
+                redirect "/adventures/#{@adventure.id}"
             end
         else
             redirect '/adventures'
         end
     end
 
+    patch '/adventures/:id' do
+        @adventure = Adventure.find_by(id: params[:id])
+        if logged_in?
+            if params[:title].empty? && params[:location].empty?
+                #You have to fill in the title & location
+                redirect "/adventures/#{@adventure.id}/edit"
+            else
+            # @adventure = Adventure.find_by(id: params[:id])
+            if @adventure && @adventure.user == current_user
+                # binding.pry
+                @adventure.update(title: params[:title], location: params[:location], activity: params[:activity], companion: params[:companion], notes: params[:notes])
+                redirect "/adventures/#{@adventure.id}"
+                # flash "Your page has been updated!"
+            else
+                redirect '/adventures'
+                # flash You cannot edit another person's adventure
+            end
+        end
+            #flash You need to be logged in to edit this page
+            redirect to '/adventures'
+        end
+    end
+    
     #make delete each adventure route
  
 
