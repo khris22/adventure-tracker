bfc5f4de4441e8c97ba1ac49c4b4bca29339eea0
Khris Punzalan <khrispunzalan@gmail.com>
2019-10-04 05:51:09 -0700

    refactored user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,39 +1,23 @@
 class UsersController < ApplicationController
 
     get '/signup' do
-        erb :"/user/signup"
-      end
+      erb :"/user/signup"
+    end
     
-      post '/user' do
-        @user = User.new(params)
-        # user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    post '/user' do
+      @user = User.new(params)
         if @user.save
           session[:user_id] = @user.id
           redirect '/adventures'
-          # "/welcome"
-          # redirect to login page, message your account has been created please log in
-        else
-            # binding.pry
-            # @user.errors.map do |keys, error|
-            #   @errors = error
-            # end
+        else           
           errors = []
           @user.errors.full_messages.map do |err|
             errors << err
-          end
-
-            # this is working
-            # all_errors = []
-            # @user.errors.messages.each do |attr, error_message|
-            #   all_errors << error_message[0]
-            # end
-            flash[:message] = errors
-
-            #  flash[:message] = "EEERRRRRROOOORRRR"
-          redirect '/signup'
-          #Please try again (make sure all the details have been filled)
         end
-      end
+            flash[:message] = errors
+            #flash[:message] = "ERROR: Please make sure all the details have been filled correctly."
+            redirect '/signup'
+        end
+    end
     
-
 end
