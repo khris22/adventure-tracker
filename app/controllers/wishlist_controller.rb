6c2e88573893399ca6b904a2e21df58333203ea9
Khris Punzalan <khrispunzalan@gmail.com>
2019-10-07 05:42:57 -0700

    created edit & delete route  on WishlistController

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -49,4 +49,43 @@ class WishlistsController < ApplicationController
                 redirect '/adventures'
             end
         end
+
+        patch '/wishlists/:id' do
+            @wishlist = Wishlist.find_by(id: params[:id])
+            if logged_in?
+                if params[:title].empty? && params[:location].empty?
+                    flash[:error] = "Title and Location cannot be empty!"
+                    redirect "/wishlists/#{@wishlist.id}/edit"
+                else      
+                    if @wishlist && @wishlist.user == current_user
+                        @wishlist.update(title: params[:title], location: params[:location], activity: params[:activity], companion: params[:companion], notes: params[:notes])
+                        flash[:message] = "Your wishlist has been updated!!!"
+                        redirect "/wishlists/#{@wishlist.id}"
+                    else
+                        flash[:error] = "You are NOT authorized to edit/delete somebody else's wishlist"
+                        redirect '/adventures'
+                    end
+                end
+                flash[:error] = "Please log in"
+                redirect to '/adventures'
+            end
+        end
+
+        delete '/wishlists/:id' do
+            if logged_in?
+                @wishlist = Wishlist.find_by(id: params[:id])
+                if @wishlist && @wishlist.user == current_user
+                    @wishlist.delete
+                    flash[:message] = "Your wishlist has been successfully deleted!"
+                    redirect to "/adventures"
+                else
+                    flash[:error] = "You are NOT authorized to edit/delete somebody else's wishlist"
+                    redirect to "/adventures"
+                end
+            else
+                flash[:error] = "Please log in"
+                redirect '/'
+            end
+        end
+
 end
